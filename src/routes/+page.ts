a09f8887dc930768dcacba1f597193f5008d8b28
DL4x <guyyouaresolucky@mail.ru>
2025-04-05 01:18:26 +0300

    Strapi added

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,16 @@
 import type { PageLoad } from './$types';
 import { Author } from '$lib';
 
-export const load: PageLoad = () => {
+export const load: PageLoad = async () => {
+	const response = await fetch('https://railway-strapi-production-7054.up.railway.app/api/persons?populate=*');// await get(ref(db, `resumes/${params.slug}`));
+
+	const { data } = await response.json();
+
+	const a = data.map(person => [person.person_name, person.id])
+
+	console.log(a);
+
 	return {
-		authors: Author.list('Balbekov Alexander', 'Gerasimov Eugene', 'Ataev Abulmuslim')
+		authors: Author.list(a)
 	};
 };
