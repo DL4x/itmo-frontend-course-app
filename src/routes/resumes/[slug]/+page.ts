6ccbadb23dca78f6741e34eb98b81acca6010a62
Alexander Balbekov <balbekov.as@mail.ru>
2025-04-05 19:15:15 +0300

    Implement proper JSON parsing based on Eugene code.
    Improve fetching single resume from strapi. Instead of fetching a whole collection, now it fetches only a single document.
    
    Stop using classes to represent data. Now Svelte components use interfaces to specify which data they require Interfaces are better, I can explain it in tg if you want.

--- src/routes/resumes/[slug]/+page.ts
+++ src/routes/resumes/[slug]/+page.ts
@@ -1,43 +1,8 @@
 import type { PageLoad } from './$types';
-import { getMyPersonById } from '$lib/connectionToDataBase';
-
-// import { db, get, ref } from '../../../../firebaseConfig';
+import { getAuthorByDocumentId } from '$lib/strapiRepository';
 
 export const load: PageLoad = async ({ params }) => {
-	try {
-		// const response = await fetch('https://railway-strapi-production-7054.up.railway.app/api/persons?populate=*');// await get(ref(db, `resumes/${params.slug}`));
-		//
-		// const { data } = await response.json();
-		//
-		// Находим конкретного пользователя по documentId из params.slug
-		// const userData = data.find(user => `${user.id}` === params.slug);
-		//
-		// if (!userData) {
-		// 	throw new Error('User not found');
-		// }
-		//
-		// Преобразование educations
-		// const educations = Education.list(
-		// 	...userData.educations.map(edu => [
-		// 		edu.education_name,
-		// 		edu.education_level,
-		// 		new TimeRange(parseInt(edu.educate_start), parseInt(edu.educate_end))
-		// 	])
-		// );
-
-		// Преобразование skills
-		// const skills = Skill.list(
-		// 	userData.skills.reduce((acc: Record<string, number>, skill) => {
-		// 		acc[skill.skill_name] = skill.skill_percent;
-		// 		return acc;
-		// 	}, {})
-		// );
-
-		return {
-			author: getMyPersonById(parseInt(params.slug))
-		};
-	} catch (error) {
-		console.error('Error loading resume:', error);
-		throw error;
-	}
+	return {
+		author: await getAuthorByDocumentId(params.slug)
+	};
 };
