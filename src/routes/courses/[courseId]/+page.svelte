e707f268eecec54e033f2bed90ad7bcdabbbc2bb
Alexander Balbekov <balbekov.as@mail.ru>
2025-04-16 18:03:28 +0300

    Add header for all pages and rewrite Resumes page on Tailwind CSS and Flowbite.
    Because I have ised Tailwind CSS, our site supports darkmode now. Only resume of concrete persons stays in light theme, because we are planning to rewrite it anyway.

--- src/routes/courses/[courseId]/+page.svelte
+++ src/routes/courses/[courseId]/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import '/src/app.css';
+	import { Heading } from 'flowbite-svelte';
 
 	const { data }: PageProps = $props();
 </script>
 <svelte:head>
 	<title>Courses</title>
 </svelte:head>
-<h1>I am page of course with id = {data.courseId}</h1>
+<Heading tag="h1">I am page of course with id = {data.courseId}</Heading>
