de081e3b758ee4a63f9c9968e4f288824bf529b4
Alexander Balbekov <balbekov.as@mail.ru>
2025-04-19 02:35:19 +0300

    Implement header and lecture choosing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
-<script>
-	import { Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+<script lang="ts">
+	import { Button, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
 	import '/src/app.css';
 	import { page } from '$app/state';
 	import VerticalLine from '$lib/VerticalLine.svelte';
@@ -7,16 +7,21 @@
 	let { children } = $props();
 </script>
 
-<Navbar class="m-auto lg:w-5xl" navContainerClass="md:justify-start gap-8">
+<Navbar class="sticky top-0 z-1000 bg-white shadow-sm" navContainerClass="gap-8">
 	<NavBrand href="/">
 		<enhanced:img class="w-20 h-20" src="/src/images/ufo.png" alt="Main logo" />
 	</NavBrand>
-	<VerticalLine height="40px"/>
-	<NavHamburger />
-	<NavUl class="order-1" activeUrl={page.url.pathname}>
-		<NavLi href="/">Main</NavLi>
-		<NavLi href="/" activeClass="">About us</NavLi>
-		<NavLi href="/" activeClass="">Support</NavLi>
+	<VerticalLine lineHeight="40px" class="not-md:hidden"/>
+
+	<NavBrand class="flex flex-row gap-4 md:ml-auto md:order-last">
+		<NavHamburger />
+		<Button>Войти</Button>
+	</NavBrand>
+	<NavUl activeUrl={page.url.pathname}>
+		<NavLi href="/">Главная</NavLi>
+		<NavLi href="/" activeClass="">Курсы</NavLi>
+		<NavLi href="/" activeClass="">О нас</NavLi>
+		<NavLi href="/" activeClass="">Поддержка</NavLi>
 	</NavUl>
 </Navbar>
 {@render children()}
--- src/routes/courses/[courseId]/+page.svelte
+++ src/routes/courses/[courseId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import '/src/app.css';
+	import PresentationPreviewCard from '$lib/PresentationPreviewCard.svelte';
 	import { Heading } from 'flowbite-svelte';
 
 	const { data }: PageProps = $props();
@@ -8,9 +9,12 @@
 <svelte:head>
 	<title>Courses</title>
 </svelte:head>
-<Heading tag="h1">I am page of course with {data.presentations.length} presentations</Heading>
-<!--<div class="flex flex-row flex-wrap gap-4 justify-center">-->
-<!--	{#each data.presentations as presentation (presentation.id)}-->
-<!--		<PresentationPreviewCard {presentation}/>-->
-<!--	{/each}-->
-<!--</div>-->
+
+<main class="flex flex-col gap-12 max-w-7xl m-auto p-4">
+	<Heading tag="h1">Наслаждайтесь лекциями от наших великолепных спикеров :-) :-) :-)</Heading>
+	<div class="flex flex-row flex-wrap gap-4 justify-around items-stretch">
+		{#each data.presentations as presentation (presentation.id)}
+			<PresentationPreviewCard {...presentation}/>
+		{/each}
+	</div>
+</main>
